ac9be9032794db197dd91c420f38be6ec9d401f3
Lucas <lucasgusso28@gmail.com>
2023-08-14 22:55:01 -0300

    Implementação de tratamentos de erro para Upload e busca de vídeos - Final do projeto

--- javaScript/buscarVideo.js
+++ javaScript/buscarVideo.js
@@ -4,6 +4,9 @@ import constroiCard from "./mostrarVideos.js";
 async function buscarVideo(evento) {
     evento.preventDefault();
 
+    const mensagemErro = document.querySelector("[data-erro]");
+    mensagemErro.innerHTML = "";    
+
     const dadosPesquisados = document.querySelector("[data-pesquisa]").value;
     const busca = await conectaApi.buscaVideo(dadosPesquisados);
 
@@ -12,8 +15,16 @@ async function buscarVideo(evento) {
     while(lista.firstChild) {
         lista.removeChild(lista.firstChild);
     }
-    
+
     busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+
+    if(busca.length == 0) {
+        mensagemErro.innerHTML =
+     `<h1 class="alinhar">
+        <span class="material-symbols-outlined">error</span>
+        <p>Não existem vídeos com esse termo.<p>
+     </h1>`;
+    }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao_pesquisa]");
